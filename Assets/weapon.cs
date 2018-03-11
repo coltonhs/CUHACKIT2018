dfdd9cf7fa7285c48c27aa9c097ac89f5d7a9203
unknown <vSpace@VSPACE04.CAMPUS.CU.CLEMSON.EDU>
2018-03-11 08:20:34 -0400

    submission

---
+++ Assets/weapon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour {
+
+	public int ammo = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.S)) {
+			Shoot();
+			Debug.Log("SHOOT");
+
+		}
+
+		if(Input.GetKeyDown(KeyCode.R)) {
+				Reload();
+				Debug.Log("RELOAD");
+			}
+			
+	}
+
+	void Shoot() {
+		if (ammo > 0) {
+						ammo = ammo - 1;
+		}
+	}
+
+	void Reload() {
+		if (ammo < 5) {
+			ammo = 5;
+		}
+	}
+}
